490ed5c03a939c897fa45f6ce9256a566ee80ea5
Vusal Mammadov <vm17041997@gmail.com>
2024-11-22 20:42:02 +0300

    Add files via upload

---
+++ sum_of_two_numbers.py
@@ -0,0 +1,11 @@
+# My dear friend this Python code calculates the sum of two numbers and prints the result.
+#Lets look 3 steps in these codes.ok? =)
+# Step 1: Initialize two variables with values
+num1 = 5  # The first number is 5
+num2 = 10  # The second number is 10
+
+# Step 2: Add the two numbers and store the result in the variable 'sum'
+sum = num1 + num2  # Adding num1 and num2
+
+# Step 3: Print the result in a formatted message
+print("Sum of", num1, "and", num2, "is:", sum)  # Output: "Sum of 5 and 10 is: 15"
